a4aaa70d1171cad694282c49c3b4c5a2dcec3034
raratii <purnamaratih419@gmail.com>
2024-10-11 09:57:50 +0700

    commit edit siakad

--- Siakadfor20.java
+++ Siakadfor20.java
@@ -9,6 +9,7 @@ public class Siakadfor20 {
         Scanner sc = new Scanner(System.in);
         
         double nilai, tertinggi = 0, terendah = 100;
+        int lulus = 0, tidakLulus = 0;
 
         for (int i = 1; i <= 10; i++) {
             System.out.print("Nilai Mahasiswa ke-" + i + ": ");
@@ -19,9 +20,16 @@ public class Siakadfor20 {
             if (nilai < terendah) {
                 terendah = nilai;
             }
+            if (nilai >= 60 ) {
+                lulus++;
+            } else{
+                tidakLulus++;
+            }
         }
         System.out.println(" Nilai Tertinggi: " +tertinggi);
         System.out.println("Nilai Terendah: " +terendah);
+        System.out.println("Jumlah siswa yang lulus: " +lulus);
+        System.out.println("Jumlah siswa yang tidak lulus: " +tidakLulus);
         
     }
 }
